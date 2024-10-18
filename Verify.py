06a8319277c85d24e8dcd37b26c1efea58f102ff
Antonio Lansang <antoniolansang500@gmail.com>
2024-10-18 13:48:11 -0500

    testing2

--- Verify.py
+++ Verify.py
@@ -1,9 +1,16 @@
-
+import os
 
 def main():
 
-    print("check")
+    #Handbrake generates logs every time you encode. Paste in where it's located on your PC
+    HandbrakeLogDirectory="C:\\Users\\Anton\\AppData\\Roaming\\HandBrake\\logs"
+    
+    #os.chdir(HandbrakeLogDirectory)
+    cwd=os.getcwd()
+    print(cwd)
 
+    #for filename in os.listdir(HandbrakeLogDirectory):
+    #    print(filename)
 
 
 
