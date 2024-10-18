4b72e82e7f54dd4a0633184a65e90c888995ee72
Antonio Lansang <antoniolansang500@gmail.com>
2024-10-18 14:18:45 -0500

    probably finished Pepega

--- Verify.py
+++ Verify.py
@@ -5,10 +5,20 @@ def main():
     #Handbrake generates logs every time you encode. Paste in where it's located on your PC
     HandbrakeLogDirectory="C:\\Users\\Anton\\AppData\\Roaming\\HandBrake\\logs"
     
+    #This is the string that will pop up numerous times in each log where subtitles are borked
+    Error="time went backwards"
+
+    #change your working directory to where the logs are stored
+    os.chdir(HandbrakeLogDirectory)
 
     for filename in os.listdir(HandbrakeLogDirectory):
-        print(filename)
+        with open(filename, 'r') as fp:
 
+            #dumps all lines into the content variable
+            content=fp.read()
+            
+            if Error in content:
+                print(filename)
 
 
 
